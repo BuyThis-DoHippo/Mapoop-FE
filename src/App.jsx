af37d2087dec91bd55cd1b5a843960393e55d7c6
chaeapark <yunapark2002@gmail.com>
2025-08-13 21:55:14 +0900

    fix: SearchToiletUrgent.jsx(긴급찾기 페이지)추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SearchStore from './pages/SearchStore';
 import SearchToilet from './pages/SearchToilet';
+import SearchToiletUrgent from './pages/SearchToiletUrgent';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/search-store" element={<SearchStore />} />
       <Route path="/find-toilet" element={<SearchToilet />} />
+      <Route path="/find-toilet/urgent" element={<SearchToiletUrgent />} />
     </Routes>
   );
 }
---
+++ src/pages/SearchToiletUrgent.jsx
@@ -0,0 +1,13 @@
+export default function SearchToiletUrgent() {
+  return (
+    <div className="w-full min-h-screen flex items-start justify-center">
+      <main className="w-full">
+        <section className="mt-[90px]">
+          <h1 className="text-[24px] font-bold text-center">
+            긴급 찾기 페이지입니다.
+          </h1>
+        </section>
+      </main>
+    </div>
+  );
+}
